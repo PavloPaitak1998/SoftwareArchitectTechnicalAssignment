80291ab0c7babb2d675fc76132652c12d7f2192c
Pavlo Paitak <pavlopaitak@gmail.com>
2020-05-15 22:53:15 +0300

    Implemented 3-layer architecture

---
+++ Test.WebApplication/Test.WebApplication.Dal/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Test.WebApplication.Dal
+{
+    public class Class1
+    {
+    }
+}
---
+++ Test.WebApplication/Test.WebApplication.UnitOfWork.Implementations/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Test.WebApplication.UnitOfWork.Implementations
+{
+    public class Class1
+    {
+    }
+}
